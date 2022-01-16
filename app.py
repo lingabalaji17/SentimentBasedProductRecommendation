ffb7e97b2ba4ff5597371a06eb4ea7b4756e7ff7
Balaji <lingabalaji17@gmail.com>
2022-01-16 21:40:19 +0530

    Updated the Userinterace with Bootsrap UI and tuned the parameters for the ML models

--- app.py
+++ app.py
@@ -1,43 +1,45 @@
-from email import message
-from flask import Flask, request, jsonify, render_template
+from email import header
+from operator import index
+from flask import Flask, request, render_template, jsonify
+from model import SentimentRecommenderModel
 
-import numpy as np
-import pandas as pd
-
-from model import SentimentModel
 
 app = Flask(__name__)
 
-sentiment_model = SentimentModel()
+sentiment_model = SentimentRecommenderModel()
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['POST'])
 def prediction():
-    #get user from the html form
-    user= request.form['userId']
-    #convert text to lowercase
-    user=user.lower()
-    #items = recommendation.getRecommendationByUser(user)
+    # get user from the html form
+    user = request.form['userName']
+    # convert text to lowercase
+    user = user.lower()
     items = sentiment_model.getSentimentRecommendations(user)
 
-    #if(~(items is None)):
-    return render_template("index.html", data=items)
-    #else:
-        #return render_template("index.html", data={message:"User Name doesn't exists, No product necommendations at this point of time!"})
+    if(not(items is None)):
+        print(f"retrieving items....{len(items)}")
+        print(items)
+        # data=[items.to_html(classes="table-striped table-hover", header="true",index=False)
+        return render_template("index.html", column_names=items.columns.values, row_data=list(items.values.tolist()), zip=zip)
+    else:
+        return render_template("index.html", message="User Name doesn't exists, No product recommendations at this point of time!")
+
 
 @app.route('/predictSentiment', methods=['POST'])
 def predict_sentiment():
-    #get the review text from the html form
-    review_text = request.form["review_text"]
+    # get the review text from the html form
+    review_text = request.form["reviewText"]
+    print(review_text)
     pred_sentiment = sentiment_model.classify_sentiment(review_text)
+    print(pred_sentiment)
     return render_template("index.html", sentiment=pred_sentiment)
 
 
 if __name__ == '__main__':
-	app.run()
-
-
+    app.run()
--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
 import pickle
 import pandas as pd
 import numpy as np
@@ -9,69 +13,92 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-import nltk
 
-class SentimentModel():
+
+class SentimentRecommenderModel:
 
     ROOT_PATH = "pickle/"
-    MODEL_NAME ="sentiment-classification-xg-boost-model.pkl"
+    MODEL_NAME = "sentiment-classification-xg-boost-model.pkl"
     VECTORIZER = "tfidf-vectorizer.pkl"
     RECOMMENDER = "user_final_rating.pkl"
+    CLEANED_DATA = "cleaned-data.pkl"
 
     def __init__(self):
-        self.model = pickle.load(open(SentimentModel.ROOT_PATH + SentimentModel.MODEL_NAME, 'rb'))
-        self.vectorizer = pd.read_pickle(SentimentModel.ROOT_PATH + SentimentModel.VECTORIZER)
-        self.user_final_rating = pickle.load(open(SentimentModel.ROOT_PATH + SentimentModel.RECOMMENDER,'rb'))
+        self.model = pickle.load(open(
+            SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.MODEL_NAME, 'rb'))
+        self.vectorizer = pd.read_pickle(
+            SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.VECTORIZER)
+        self.user_final_rating = pickle.load(open(
+            SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.RECOMMENDER, 'rb'))
         self.data = pd.read_csv("dataset/sample30.csv")
+        self.cleaned_data = pickle.load(open(
+            SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.CLEANED_DATA, 'rb'))
         self.lemmatizer = WordNetLemmatizer()
         self.stop_words = set(stopwords.words('english'))
 
+    """function to get the top product 20 recommendations for the user"""
+
     def getRecommendationByUser(self, user):
         recommedations = []
-        #try:
         return list(self.user_final_rating.loc[user].sort_values(ascending=False)[0:20].index)
-        #except:
-           # print("No user exists")
+
+    """function to filter the product recommendations using the sentiment model and get the top 5 recommendations"""
 
     def getSentimentRecommendations(self, user):
-        recommendations = self.getRecommendationByUser(user)
-        #if(~(recommendations is None)):
-        temp = self.data[self.data.id.isin(recommendations)]
-        #temp["reviews_text_cleaned"] = temp["reviews_text"].apply(lambda x: self.preprocess_text(x))
-        X = self.vectorizer.transform(temp["reviews_text"].values.astype(str))
-        temp["prediction"]= self.model.predict(X)
-        temp = temp[['name','prediction']]
-        temp=temp.groupby('name').sum()
-        temp['positive_percent']=temp.apply(lambda x: x['prediction']/sum(x), axis=1)
-        final_list=list(temp.sort_values('positive_percent', ascending=False)[0:5].index)
-        return final_list
+        if (user in self.user_final_rating.index):
+            # get the product recommedation using the trained ML model
+            recommendations = list(
+                self.user_final_rating.loc[user].sort_values(ascending=False)[0:20].index)
+            filtered_data = self.cleaned_data[self.cleaned_data.id.isin(
+                recommendations)]
+            # preprocess the text before tranforming and predicting
+            #filtered_data["reviews_text_cleaned"] = filtered_data["reviews_text"].apply(lambda x: self.preprocess_text(x))
+            # transfor the input data using saved tf-idf vectorizer
+            X = self.vectorizer.transform(
+                filtered_data["reviews_text_cleaned"].values.astype(str))
+            filtered_data["predicted_sentiment"] = self.model.predict(X)
+            temp = filtered_data[['id', 'predicted_sentiment']]
+            temp_grouped = temp.groupby('id', as_index=False).count()
+            temp_grouped["pos_review_count"] = temp_grouped.id.apply(lambda x: temp[(
+                temp.id == x) & (temp.predicted_sentiment == 1)]["predicted_sentiment"].count())
+            temp_grouped["total_review_count"] = temp_grouped['predicted_sentiment']
+            temp_grouped['pos_sentiment_percent'] = np.round(
+                temp_grouped["pos_review_count"]/temp_grouped["total_review_count"]*100, 2)
+            sorted_products = temp_grouped.sort_values(
+                'pos_sentiment_percent', ascending=False)[0:5]
+            return pd.merge(self.data, sorted_products, on="id")[["name", "brand", "manufacturer", "pos_sentiment_percent"]].drop_duplicates().sort_values('pos_sentiment_percent', ascending=False)
+
+        else:
+            print(f"User name {user} doesn't exist")
+            return None
+
+    """function to classify the sentiment to 1/0 - positive or negative - using the trained ML model"""
 
     def classify_sentiment(self, review_text):
-        sentiment_text = ""
         review_text = self.preprocess_text(review_text)
         X = self.vectorizer.transform([review_text])
         y_pred = self.model.predict(X)
         return y_pred
 
+    """function to preprocess the text before it's sent to ML model"""
+
     def preprocess_text(self, text):
 
-        #cleaning the review text (lower, removing punctuation, numericals, whitespaces)
+        # cleaning the review text (lower, removing punctuation, numericals, whitespaces)
         text = text.lower().strip()
         text = re.sub("\[\s*\w*\s*\]", "", text)
         dictionary = "abc".maketrans('', '', string.punctuation)
         text = text.translate(dictionary)
         text = re.sub("\S*\d\S*", "", text)
 
-        #remove stop-words and convert it to lemma
-        text = self.lemma_text(text)
+        # remove stop-words and convert it to lemma
+        #text = self.lemma_text(text)
+        # remove stop-words
+        text = self.remove_stopword(text)
         return text
-    
 
-    # This is a helper function to map NTLK position tags
+    """function to get the pos tag to derive the lemma form"""
+
     def get_wordnet_pos(self, tag):
         if tag.startswith('J'):
             return wordnet.ADJ
@@ -83,13 +110,20 @@ class SentimentModel():
             return wordnet.ADV
         else:
             return wordnet.NOUN
-    
+
+    """function to remove the stop words from the text"""
+
     def remove_stopword(self, text):
-        words = [word for word in text.split() if word.isalpha() and word not in self.stop_words]
+        words = [word for word in text.split() if word.isalpha()
+                 and word not in self.stop_words]
         return " ".join(words)
 
+    """function to derive the base lemma form of the text using the pos tag"""
+
     def lemma_text(self, text):
-        word_pos_tags = nltk.pos_tag(word_tokenize(self.remove_stopword(text))) # Get position tags
+        word_pos_tags = nltk.pos_tag(word_tokenize(
+            self.remove_stopword(text)))  # Get position tags
         # Map the position tag and lemmatize the word/token
-        words =[self.lemmatizer.lemmatize(tag[0], self.get_wordnet_pos(tag[1])) for idx, tag in enumerate(word_pos_tags)] 
+        words = [self.lemmatizer.lemmatize(tag[0], self.get_wordnet_pos(
+            tag[1])) for idx, tag in enumerate(word_pos_tags)]
         return " ".join(words)
